491f551005d9118e17534a549d2102dbd4f8adc2
HexagramNM <hexagram_n.m@jcom.home.ne.jp>
2018-07-09 17:19:28 +0900

    Twitter APIのDM周りの仕様変更に伴い，ライブラリがまだ対応していない可能性も考慮し，windows側の通知機能を利用するように実装を変更

--- LockedFollowerTeller.py
+++ LockedFollowerTeller.py
@@ -3,12 +3,17 @@ import sys
 import os.path
 import traceback
 from twitter import *
+from time import sleep
+from win10toast import ToastNotifier
+from datetime import datetime
 commandline_args = sys.argv
 commandline_argc = len(sys.argv)
 #下のアカウント名を自分自身のものに変更すること
 account_screen_name = "HexagramNM"
 
 log_file = open("error_log.txt", "a")
+follower_log = open("follower_log.txt", "a");
+toaster = ToastNotifier()
 sys.stderr = log_file
 try:
     with open("secret.json") as f:
@@ -17,7 +22,7 @@ try:
     t = Twitter(auth=OAuth(secretjson["access_token"], secretjson["access_token_secret"], secretjson["consumer_key"], secretjson["consumer_secret"]))
     newfollowerlists = t.followers.ids(screen_name=account_screen_name, stringify_ids=True, account=5000)['ids']
     if commandline_argc == 2 and commandline_args[1] == '-init':
-        followerfile = open("followerlists.txt", "a")
+        followerfile = open("followerlists.txt", "w")
         for id_str in newfollowerlists:
             followerfile.write(id_str + '\n')
         followerfile.close()
@@ -47,11 +52,14 @@ try:
                     old_account = t.users.lookup(user_id=old_id)
                     DM_string = DM_string + old_account[0]['name'] + "さん（@" + old_account[0]['screen_name'] + "）からのフォローが解除されました．\n"
                 except:
+                    sys.stderr.write(traceback.format_exc())
                     delete_account_num += 1
         if delete_account_num > 0:
             DM_string = DM_string + "その他，" + str(delete_account_num) + "人のフォロワーが削除されています．\n"
         if follower_elase:
-            t.direct_messages.new(screen_name=account_screen_name, text=DM_string)
+            toaster.show_toast("フォロー解除検知", DM_string, icon_path=None, duration=10, threaded=True)
+            follower_log.write(datetime.now().strftime("%Y/%m/%d %H:%M:%S") + "->\n" + DM_string + "\n")
+            #t.direct_messages.new(screen_name=account_screen_name, text=DM_string)
 
         DM_string = ""
         for new_id in newfollowerlists:
@@ -61,14 +69,16 @@ try:
                     DM_string = DM_string + "鍵垢の" + new_account[0]['name'] + "さん (@" + new_account[0]['screen_name'] + ") にフォローされました．\n"
                     newLockedUserExist = True
         if newLockedUserExist:
-            t.direct_messages.new(screen_name=account_screen_name, text=DM_string)
+            toaster.show_toast("鍵垢フォロー検知", DM_string, icon_path=None, duration=10, threaded=True)
+            follower_log.write(datetime.now().strftime("%Y/%m/%d %H:%M:%S") + "->\n" + DM_string + "\n")
+            #t.direct_messages.new(screen_name=account_screen_name, text=DM_string)
 
-    if(newLockedUserExist):
-        t.statuses.update(status="@" + account_screen_name + "\n通知：新しい鍵垢フォロワーを確認．\n推奨：DMの確認．")
     datefile = open("followerlists.txt", "w")
     for line in newfollowerlists:
         datefile.write(line + '\n')
     datefile.close()
+    while toaster.notification_active():
+        sleep(0.1)
 except:
     sys.stderr.write(traceback.format_exc())
 log_file.close()
